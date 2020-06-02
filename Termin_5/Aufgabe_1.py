9db4ab59a3b35cf2d22df2e13f47c61220b709f3
Danila <you@example.com>
2020-06-02 13:34:09 +0200

    Second Commit, let's see how it works

---
+++ Termin_5/Aufgabe_1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+@author: kerstin
+"""
+
+liste = [6, -1, 9, -18, 19, 15, 3, 17, 26, 15]
+print(liste)
+
+def swap(li,i,j):
+    li[i], li[j] = li[j], li[i]
+    return li
+
+def minPos(li, lo, hi):
+    min = lo
+    for i in range(lo+1,hi+1):
+        if li[i]<li[min]:
+            min = i
+    return min
+
+def selectionSort(li):
+    hi = len(li)-1
+    for k in range(hi):
+        min = minPos(li,k,hi)
+        if min != k:
+            swap(li,min,k)
+    return li
+
+print(selectionSort(liste))
